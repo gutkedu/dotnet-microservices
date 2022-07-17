3cf9ad55cc8e120828a0ac9bd219e34ed5047c5a
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-07-17 17:39:33 -0300

    🐞 fix: mongodb document name

--- Play.catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
+++ Play.catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -15,7 +15,7 @@ namespace Play.Catalog.Service.Repositories
     public ItemsRepository()
     {
       var mongoClient = new MongoClient("mongodb://localhost:27017");
-      var database = mongoClient.GetDatabase("Catalog");
+      var database = mongoClient.GetDatabase("catalog");
       dbCollection = database.GetCollection<Item>(collectionName);
     }
 
